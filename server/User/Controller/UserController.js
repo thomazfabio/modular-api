73fa52d1c32fb2223399b58fdd4677cb4db56248
fabio thomaz <fabio_thomaz@live.com>
2022-05-25 19:22:26 -0400

    trabalhando na rota newuser

--- server/User/Controller/UserController.js
+++ server/User/Controller/UserController.js
@@ -1,20 +1,22 @@
 const UserModel = require('../Model/UserModel') //importa o model user
+const { use } = require('../Routes/UserRoutes')
 
 module.exports = {
     setuser: (req, res) => {
         //criando nova instancia de User
         //dados para o User ( _Id, _first_name, _last_name, _email, _password, _phone) 
         //!Atenção valores não passados serão undefned e não serão nulo!
-        //let user = req.body
-       // newUser = new UserModel(user.Id, user.first_name, user.last_name, user.email, user.password, user.phone)
-       x =  new UserModel.User(req.body.Id)
-    
-        console.log(x)
-        res.send(x);
+        let user = req.body
+        UserN = new UserModel.User(user.Id, user.first_name, user.last_name, user.email, user.password, user.phone)
+        let users = []
+        users.push(UserN)
+        console.log(UserN)
+
+        res.send(users);
     },
 
     getuser: (req, res) => {
-        res.send(newUser)
+        res.send(UserModel)
     }
 }
 
